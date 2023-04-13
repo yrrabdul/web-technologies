fb79764b8efc059e28a4e06c82229f15c63b7b2a
Abdul Rehman <72547342+ABD-UL-REHMAN355@users.noreply.github.com>
2023-04-13 13:55:05 +0500

    midterm exam done

---
+++ labmidterm/javascript.js
@@ -0,0 +1,13 @@
+
+function showPopup() {
+    document.getElementById("popup-banner").style.display = "block";
+  }
+  
+
+  function closePopup() {
+    document.getElementById("popup-banner").style.display = "none";
+  }
+  
+  window.onload = function() {
+    showPopup();
+  };
